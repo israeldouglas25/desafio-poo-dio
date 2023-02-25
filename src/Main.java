05056a6785f95addd381a0ad94f115548fcd3bdc
Israel <isreldouglas25@gmail.com>
2023-02-25 15:58:57 -0300

    class conteudo e ajuste nas class filhas curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.domain.Curso;
+import br.com.dio.desafio.domain.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Curso Java");
+        curso1.setCargaHoraria(80);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Angula");
+        curso2.setDescricao("Curso Angula");
+        curso2.setCargaHoraria(40);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Mentoria da aula de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
     }
 }
---
+++ src/br/com/dio/desafio/domain/Mentoria.java
@@ -0,0 +1,37 @@
+package br.com.dio.desafio.domain;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo {
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public Mentoria(LocalDate data) {
+        this.data = data;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + data +
+                '}';
+    }
+}
